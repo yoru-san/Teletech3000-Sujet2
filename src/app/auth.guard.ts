98459dcdb26cbb4e6d9309d209787afc9dc1ba74
Corneau Fabio <fabio.corneau@ynov.com>
2018-06-13 14:39:31 +0200

    navbar bug fixed

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -13,6 +13,6 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    return localStorage.loggedIn;
   }
 }
